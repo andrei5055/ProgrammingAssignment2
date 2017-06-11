ef3c162c9815bc482a452e46fe7d9b4b94a8801b
Andrei Ivanov <Andrey.Ivanov@gmail.com>
2017-06-11 12:33:54 -0700

    Implementation
    Implementation  of the makeCacheMatrix & cacheSolve methods

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix - creates an object of the matrix type, which allows  
+##                   to use the cached results of its inversion
+## NOTE: the input parameter x could be any (n x m) matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inversedMatrix <- NULL
+    
+    # Create list of methods for manipulation with the matrix
+    list(set = function(y) { x <<- y; inversedMatrix <<- NULL}, 
+         get = function() x,
+         setInversedMatrix = function(invMatr) inversedMatrix <<- invMatr,
+         getInversedMatrix = function() inversedMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - inverts the matrix, created by makeCacheMatrix
+## NOTE: this function will try to invert only the square matrices of any size
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invMatr <- x$getInversedMatrix()
+    if(!is.null(invMatr)) {
+        return(invMatr)  # Getting cached data
+    }
+
+    # Check if matrix is a square matrix 
+    y <- x$get()
+    len <- length(y) 
+    size <- sqrt(len)
+    if (len != size * size) {
+        message("Cannot invert matrix - it is not a square matrix")
+        return(NaN)
+    }
+    
+    # Calculate, store and return the inverse matrix
+    invMatr <- solve(y, diag(size), ...)
+    x$setInversedMatrix(invMatr)
+    invMatr
 }
